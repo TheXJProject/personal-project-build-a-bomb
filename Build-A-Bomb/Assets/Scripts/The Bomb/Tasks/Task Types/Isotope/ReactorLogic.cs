80dd1390b41b8875f0512d5eda2d9a6e85a909ff
Steampowered85 <adamclaridge2000@gmail.com>
2024-11-17 23:01:55 +0000

    Started reactor animation

--- Build-A-Bomb/Assets/Scripts/The Bomb/Tasks/Task Types/Isotope/ReactorLogic.cs
+++ Build-A-Bomb/Assets/Scripts/The Bomb/Tasks/Task Types/Isotope/ReactorLogic.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class ReactorLogic : MonoBehaviour
 {
+    // ==== For Debugging ====
+    readonly bool Msg = false;
 
+    // Inspector Adjustable Values:
+    [SerializeField] [Range(0f, 5f)] float baseFanSpeed = 0;
+
+    // Initialise In Inspector:
+    [SerializeField] GameObject fan;
+
+    // Runtime Variables:
+    [HideInInspector] public bool canSpool;
+
+    private void Awake()
+    {
+        canSpool = true;
+
+        // Start fan at random angle
+        fan.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0f, 360f));
+    }
+
+    private void FixedUpdate()
+    {
+        if (canSpool)
+        {
+            fan.transform.Rotate(0, 0, baseFanSpeed);
+        }
+    }
 }

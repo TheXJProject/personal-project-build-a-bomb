3bbcbfb8383df5dde705fa20923d56c6300d496f
Alex Claridge <thexjprojectac@gmail.com>
2025-08-07 19:33:20 +0100

    Added all bomb backgrounds

---
+++ Build-A-Bomb/Assets/Scripts/The Bomb/Layers/layerGlow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layerGlow : MonoBehaviour
+{
+    [SerializeField] float spinSpeed = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the object around its Y axis (you can change this to X or Z for other directions)
+        transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+    }
+}

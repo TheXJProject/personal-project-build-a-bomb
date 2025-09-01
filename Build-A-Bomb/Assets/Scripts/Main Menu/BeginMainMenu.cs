002a0f7b537b7889c72e5ea7c630ea6834f64860
Steampowered85 <adamclaridge2000@gmail.com>
2025-09-01 18:45:21 +0100

    Changes for final pre alpha build

--- Build-A-Bomb/Assets/Scripts/Main Menu/BeginMainMenu.cs
+++ Build-A-Bomb/Assets/Scripts/Main Menu/BeginMainMenu.cs
@@ -16,27 +16,28 @@ public class BeginMainMenu : MonoBehaviour
 
     private void Start()
     {
-        double startTime = AudioSettings.dspTime + startMusicTime;
+        // TODO:: readd music
+        //double startTime = AudioSettings.dspTime + startMusicTime;
 
-        // Set all music groups to zero volume
-        MixerFXManager.instance.ForceSetParam(GROUP_OPTIONS.MUSIC_COLLECTION, EX_PARA.VOLUME, 0f);
+        //// Set all music groups to zero volume
+        //MixerFXManager.instance.ForceSetParam(GROUP_OPTIONS.MUSIC_COLLECTION, EX_PARA.VOLUME, 0f);
 
-        // Play all main menu music tracks at the same time
-        AudioManager.instance.PlayMusic("Menu Alarms", startTime);
-        AudioManager.instance.PlayMusic("Menu Bass", startTime);
-        AudioManager.instance.PlayMusic("Menu Beeps", startTime);
-        AudioManager.instance.PlayMusic("Menu Choir", startTime);
-        AudioManager.instance.PlayMusic("Menu FullChoirCrash", startTime);
-        AudioManager.instance.PlayMusic("Menu Hats", startTime);
-        AudioManager.instance.PlayMusic("Menu KickSnare", startTime);
-        AudioManager.instance.PlayMusic("Menu OfficeNoise", startTime);
-        AudioManager.instance.PlayMusic("Menu Organ", startTime);
-        AudioManager.instance.PlayMusic("Menu StartMelody", startTime);
-        AudioManager.instance.PlayMusic("Menu StringsXyphone", startTime);
+        //// Play all main menu music tracks at the same time
+        //AudioManager.instance.PlayMusic("Menu Alarms", startTime);
+        //AudioManager.instance.PlayMusic("Menu Bass", startTime);
+        //AudioManager.instance.PlayMusic("Menu Beeps", startTime);
+        //AudioManager.instance.PlayMusic("Menu Choir", startTime);
+        //AudioManager.instance.PlayMusic("Menu FullChoirCrash", startTime);
+        //AudioManager.instance.PlayMusic("Menu Hats", startTime);
+        //AudioManager.instance.PlayMusic("Menu KickSnare", startTime);
+        //AudioManager.instance.PlayMusic("Menu OfficeNoise", startTime);
+        //AudioManager.instance.PlayMusic("Menu Organ", startTime);
+        //AudioManager.instance.PlayMusic("Menu StartMelody", startTime);
+        //AudioManager.instance.PlayMusic("Menu StringsXyphone", startTime);
 
-        // Fade in the start menu tracks
-        MixerFXManager.instance.SetMusicParam("Menu StartMelody", EX_PARA.VOLUME, 0.1f);
-        MixerFXManager.instance.SetMusicParam("Menu Bass", EX_PARA.VOLUME, 0.1f);
+        //// Fade in the start menu tracks
+        //MixerFXManager.instance.SetMusicParam("Menu StartMelody", EX_PARA.VOLUME, 0.1f);
+        //MixerFXManager.instance.SetMusicParam("Menu Bass", EX_PARA.VOLUME, 0.1f);
     }
 
     private void OnMouseDown()
